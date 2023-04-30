15f808a46dcf0ac5500cbf2e16b99c64d18e2119
Abhinav Kumar <114800265+abhinav900@users.noreply.github.com>
2023-04-30 14:33:43 +0530

    For loop in c language

---
+++ loop.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+//void main(){
+//int i,j;
+//for(i=0;i<=3;i++){
+//	for(j=0;j<=3;j++){
+//		printf("%d,%d\n",i,j);
+//	}
+//}	
+//}
+
+//
+//void main(){
+//	int i,j;
+//	for(i=1;j<=5;i++){
+//		for(j=1;j<=i;j++){
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+
+//void main(){
+//	int i,j;
+//	for (i=1;j<=5;i++){
+//		for(j=1;j<=i;j++){
+//			printf("%d",j);
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+//void main(){
+//	int i,j;
+//	for(i=1;j<=5;i++){
+//		for(j=1;j<=5;j++){
+//			printf("%d",j);
+//	}
+//	printf("\n");
+//	}
+//}
+
+
+//void main(){
+//	int i,j;
+//	for(i=5;i>=1;i--){
+//		for(j=5;j>=1;j--){
+//			printf("%d",j);
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+//void main(){
+//	int i,j;
+//	for(i=5;i>=1;i--){
+//		for(j=i;j>=1;j--){
+//			printf("%d",j);
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+void main(){
+	printf("%5d\n",1);
+	printf("%5d\n",12);
+	printf("%5d\n",123);
+	printf("%5d\n",1234);
+	printf("%5d\n",12345);
+}
